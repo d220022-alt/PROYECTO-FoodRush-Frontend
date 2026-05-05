cf6f72e6fa90bc79163ccd50b90e4b789f5771dd
yirbert1 <yirbertsu18@gmail.com>
2026-05-05 11:01:22 -0400

    Your current version of Antigravity is out of date. Please visit https://antigravity.google/download to download and install the latest version.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 import { createApp } from 'vue'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './style.css'
-import App from './views/delivery.vue'
+import App from './App.vue'
 import router from './router'
 import { initializeTheme } from './services/theme'
 
